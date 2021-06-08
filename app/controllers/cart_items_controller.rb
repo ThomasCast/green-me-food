33525335f0c257f4bb4ba4280c2fdf0cbb2f7a7b
Thomas Castaldi <tomas2mars@hotmail.fr>
2021-06-08 15:52:24 +0200

    controllers created

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,2 +1,27 @@
 class CartItemsController < ApplicationController
+
+  def create
+    @cart_item = Cart_item.new(cart_item_params)
+    @product = Product.find(params[:product_id])
+    @cart_item.product = @product
+    @cart_item.save
+    redirect_to cart_path
+  end
+
+  def update
+    @cart_item = Cart_item.find(params[:id])
+    @cart_item.update(cart_item_params)
+    redirect_to cart_path
+  end
+  
+  def destroy
+    @cart_item = Cart_item.find(params[:id])
+    @cart_item.destroy
+    redirect_to cart_path
+  end
+  
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:quantity, :buying_frequency, :user_id, :product_id)
 end
