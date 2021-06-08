0de332c1844908381958cacea821282df1687e2f
Thomas Castaldi <tomas2mars@hotmail.fr>
2021-06-08 12:55:32 +0200

    wishlist model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_08_105030) do
+ActiveRecord::Schema.define(version: 2021_06_08_105445) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -92,9 +92,20 @@ ActiveRecord::Schema.define(version: 2021_06_08_105030) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "wishlists", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "product_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["product_id"], name: "index_wishlists_on_product_id"
+    t.index ["user_id"], name: "index_wishlists_on_user_id"
+  end
+
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "cart_items", "products"
   add_foreign_key "cart_items", "users"
   add_foreign_key "products", "categories"
   add_foreign_key "products", "producers"
+  add_foreign_key "wishlists", "products"
+  add_foreign_key "wishlists", "users"
 end
