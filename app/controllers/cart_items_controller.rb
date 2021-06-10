ac7535d2b229c13d820facd5a6425eebe1c3e51d
Thomas Castaldi <tomas2mars@hotmail.fr>
2021-06-10 16:47:13 +0200

    add cart item

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,21 +1,28 @@
+require "pry-byebug"
+
 class CartItemsController < ApplicationController
 
   def create
-    @cart_item = Cart_item.new(cart_item_params)
-    @product = Product.find(params[:product_id])
-    @cart_item.product = @product
-    @cart_item.save
-    redirect_to cart_path
+    @cart_item = CartItem.new(cart_item_params)
+    @product = @cart_item.product
+    @cart_item.user = current_user
+  
+    if @cart_item.save
+      respond_to do |format|
+        format.html { redirect_to cart_path  }
+        format.js { render :create}
+      end
+    end
   end
 
   def update
-    @cart_item = Cart_item.find(params[:id])
+    @cart_item = CartItem.find(params[:id])
     @cart_item.update(cart_item_params)
     redirect_to cart_path
   end
 
   def destroy
-    @cart_item = Cart_item.find(params[:id])
+    @cart_item = CartItem.find(params[:id])
     @cart_item.destroy
     redirect_to cart_path
   end
@@ -23,6 +30,6 @@ class CartItemsController < ApplicationController
   private
 
   def cart_item_params
-    params.require(:cart_item).permit(:quantity, :buying_frequency, :user_id, :product_id)
+    params.require(:cart_item).permit(:quantity, :buying_frequency, :product_id)
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,12 @@
 class Product < ApplicationRecord
   belongs_to :category
   belongs_to :producer
+  has_many :cart_items
+
+  def is_in_user_cart?(user)
+    self.cart_items.any? do |cart_item|
+      cart_item.user == user
+    end
+  end
+
 end
