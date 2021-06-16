a0d0640be28aff3468dc0dd7f260f4dd18268670
theophile cestaro <theophilecestaro96@gmail.com>
2021-06-16 12:57:00 +0200

    cuuuuustooome

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ApplicationRecord
   has_many :cart_items
 
   def total_cart
-    cart_items.reduce(0) do |sum, cart_item| 
-      (sum + (cart_item.quantity * cart_item.product.price * cart_item.buying_frequency)) / 1.00
+    cart_items.reduce(0) do |sum, cart_item|
+      ((sum + (cart_item.quantity * cart_item.product.price * cart_item.buying_frequency)) / 1.00).round(2)
     end
   end
 
