4ea941de5e0ac43a0a7f108ac3057dadce4611ef
Thomas Castaldi <tomas2mars@hotmail.fr>
2021-06-17 11:09:48 +0200

    Add work to Producers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_14_104544) do
+ActiveRecord::Schema.define(version: 2021_06_17_090731) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 2021_06_14_104544) do
     t.string "photo_url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "work"
   end
 
   create_table "products", force: :cascade do |t|
